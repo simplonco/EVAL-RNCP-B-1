2ece26c2e55ffd76276ef470c1c2eea6fab10591
amare16 <amarenighatu16@gmail.com>
2016-10-17 15:42:34 +0200

    white start up

---
+++ checking_mail.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--<link href="css/bootstrap.min.css" rel="stylesheet">-->
+    <link href="css/custom.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <title>Checking Mail in Secours Catholique</title>
+</head>
+<body>
+    <div id="header">
+        <img id="logo" alt="secours_catholique" src="images/logo_secours_catholique.png" />
+    </div>
+</body>
+</html>
---
+++ new_project.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>New Project for BAC</title>
+    <meta charset="utf-8">
+    <link href="style.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+  </head>
+  <body>
+    <?php
+      $yourId = "Your Id";
+      $name = "name";
+
+    ?>
+    <form action="" method="post">
+      <input type="" placeholder="Your Id" value=""/>
+      <input type="text" name="name" value="" placeholder="name" />
+      <input type="button" name="" value="Submit">
+    </form>
+  </body>
+</html>
